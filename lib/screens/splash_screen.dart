a5059ea2810a4e7c7d14e445c0c879e509581809
tkb <pingpong.mix.tkb@gmail.com>
2025-02-12 02:41:58 +0900

    [fix]splashScreenの不要なdelayを削除

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,21 +1,27 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
-import 'package:pingpong_mix/providers/auth_provider.dart';
+import '../viewmodels/auth_viewmodel.dart';
 
 class SplashScreen extends ConsumerWidget {
   const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    Future.delayed(const Duration(seconds: 2), () {
-      final authState = ref.read(authProvider); // AuthState を取得
-      //TODO: 必ずhomeを表示するようにしているため、元に戻す
-      if (authState.user != null) {
-        // AuthState 内の user が null かどうかを判定
-        context.go('/home/matching');
-      } else {
-        context.go('/auth');
+    ref.listen(authViewModelProvider, (previous, next) {
+      if (next is AsyncLoading) {
+        // ローディング中は何もしない
+        return;
+      }
+
+      if (next is AsyncData<User?>) {
+        final user = next.value;
+        if (user != null) {
+          context.go('/home/matching');
+        } else {
+          context.go('/auth');
+        }
       }
     });
 
